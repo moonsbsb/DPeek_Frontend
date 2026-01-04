d1e514efcb18d7316650e914cc9cf80ddb20ad33
moon890309 <abcdefg8989@naver.com>
2026-01-04 18:37:25 +0900

    feat: 온보딩 시작 화면

--- lib/screen/onboard/Start1Page.dart
+++ lib/screen/onboard/Start1Page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:mypal/theme/appcolor.dart';
 
 class Start1Page extends StatelessWidget {
 
@@ -12,40 +13,60 @@ class Start1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          children: [
-            const Spacer(),
-
-            Text(
-              '마이팔',
-              style: Theme.of(context).textTheme.headlineLarge,
-              textAlign: TextAlign.center,
-            ),
-
-            const SizedBox(height: 16),
-
-            SvgPicture.asset(
+    return Stack(
+      children: [
+        Container(
+          color: Appcolor.background,
+        ),
+        Positioned(
+          left: 0,
+            right: 0,
+            bottom: 0,
+            child: SvgPicture.asset(
               'assets/images/opening_background.svg',
               fit: BoxFit.contain,
             ),
-
-            const Spacer(),
-
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: onNext,
-                child: const Text('시작하기'),
+          ),
+          SafeArea(
+            child: Stack(
+            children: [
+              Align(
+                alignment: const FractionalOffset(0.5, 0.2),
+                child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Text(
+                    'D-Peek',
+                    style: Theme.of(context).textTheme.headlineLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                children: [
+                const Spacer(),
 
-            const SizedBox(height: 32),
-          ],
+                  SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                  onPressed: onNext,
+                  child: const Text('시작하기'),
+                ),
+              ),
+
+              const SizedBox(height: 32),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
+    
     );
   }
 }
+
+
+
