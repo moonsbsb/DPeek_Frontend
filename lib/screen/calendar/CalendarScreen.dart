4bdd9016be328571a9cfc31bef2f76fcc72396c8
moon890309 <abcdefg8989@naver.com>
2026-01-06 13:29:26 +0900

    feat: 바턴 네비게이션 바 구현

---
+++ lib/screen/calendar/CalendarScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class CalendarScreen extends StatelessWidget{
+  const CalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('달력'),
+    );
+  }
+}
---
+++ lib/screen/report/ReportScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ReportScreen extends StatelessWidget{
+  const ReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('분석'),
+    );
+  }
+}
